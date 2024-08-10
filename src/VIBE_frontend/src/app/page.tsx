526538038a2e281c9a5147e3ea8bde520bf511ac
Benedict Contawe <benedict.contawe@ibm.com>
2024-08-10 22:34:43 +0800

    Landing Page UI update, Prisma Update Version

--- src/VIBE_frontend/src/app/page.tsx
+++ src/VIBE_frontend/src/app/page.tsx
@@ -3,7 +3,17 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Click Me</Button>
+      {/* <img src={imageSrc} alt="Volunteerism Image" className="image-style" /> */}
+      <p>Making <b>Volunteerism</b> More Exciting</p>
+      <p>Join the force to create a fun and exciting experience</p>
+      <p>Increaser in <b>Action</b></p>
+      <p>Welcome to your</p>
+      <p>Partner In <b>Volunteerism</b></p>
+      <p>VIBE (Volunteer Impact Blockchain Ecosystem) tackles the challenge of low volunteer engagement by making volunteering more fun and rewarding. Through a gamified approach, volunteers earn digital tokens for their contributions and task completion. Smart contracts ensure secure and transparent management of both internal funds and crypto donations from supporters. This system benefits everyone involved. Volunteers gain valuable networking opportunities while earning tokens, and organizations can build a strong community by connecting with active volunteers. VIBE essentially hits two birds with one stone – making volunteering enjoyable and fostering stronger connections within the community.</p>
+      <p><b>Questions?</b> We're Here to Help</p>
+      <p>Be Heard</p>
+      <Button>Sign up</Button>
+      <p>VeeBy Software Development Services 2024</p>
     </main>
   );
 }
