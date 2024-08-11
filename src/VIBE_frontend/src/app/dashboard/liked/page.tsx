c037c7189b94018173eccb652f43640bffe0cf57
jacegonzales25 <jacegonzales25@gmail.com>
2024-08-11 18:26:08 +0800

    feat: add liked events

--- src/VIBE_frontend/src/app/dashboard/liked/page.tsx
+++ src/VIBE_frontend/src/app/dashboard/liked/page.tsx
@@ -1,18 +1,60 @@
-'use client';
-import React from 'react';
-import Chip from '@/components/chip/Chip';
+"use client";
+import React, { useEffect } from "react";
+import Chip from "@/components/chip/Chip";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import {
+  fetchUserEvents,
+  fetchUserLikedEvents,
+  fetchUserDetails,
+} from "@/lib/features/auth/userSlice";
 
 const Liked: React.FC = () => {
-  const chips = ['Technology', 'Business', 'Marketing', 'Education', 'Arts'];
+  const dispatch = useAppDispatch();
+  const { id, token, userDetails, userEvents, userLikedEvents, status, error } =
+    useAppSelector((state) => state.user);
+
+  useEffect(() => {
+    if (id && token) {
+      dispatch(fetchUserDetails());
+      dispatch(fetchUserEvents());
+      dispatch(fetchUserLikedEvents());
+    }
+  }, [dispatch, id, token]);
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "failed") {
+    return <div>Error: {error}</div>;
+  }
+
+  const chips = ["Technology", "Business", "Marketing", "Education", "Arts"];
   const handleChipClick = (chip: string) => {
     alert(`Chip clicked: ${chip}`);
   };
   return (
-    <div className='text-lg m-2'>
-      <h1 className='font-bold'>Liked</h1>
-      <div className='flex justify-center mt-4'>
+    <div className="text-lg m-2">
+      <h1 className="font-bold">Liked</h1>
+      <div className="flex justify-center mt-4">
         {chips.map((chip, index) => (
-          <Chip key={index} label={chip} onClick={() => handleChipClick(chip)} />
+          <Chip
+            key={index}
+            label={chip}
+            onClick={() => handleChipClick(chip)}
+          />
+        ))}
+      </div>
+      <h2 className="font-bold mt-4">Liked Events</h2>
+      <div className="mt-4">
+        {userLikedEvents.map((event) => (
+          <div key={event.id} className="mb-4 p-4 bg-gray-800 rounded-md">
+            <h3 className="text-xl font-bold">{event.title}</h3>
+            <p>{event.description}</p>
+            <p>Location: {event.location}</p>
+            <p>Start Date: {new Date(event.startDate).toLocaleDateString()}</p>
+            <p>End Date: {new Date(event.endDate).toLocaleDateString()}</p>
+          </div>
         ))}
       </div>
     </div>
