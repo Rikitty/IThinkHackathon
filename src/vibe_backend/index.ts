55513cc1aedb649548e1d14808412d760ddf48aa
jacegonzales25 <jacegonzales25@gmail.com>
2024-09-02 15:15:47 +0800

    feat: finish backend

---
+++ src/vibe_backend/index.ts
@@ -0,0 +1,73 @@
+import {
+  Server,
+  init,
+  postUpgrade,
+  preUpgrade,
+  setNodeServer,
+} from "azle/experimental";
+
+import "reflect-metadata";
+
+import { Database, DatabaseOptions } from "./database";
+import { ENTITIES } from "./database/entities";
+import { ConsoleLogger } from "./database/logger";
+import { DatabaseStorage } from "./database/storage";
+import { CreateServer } from "./server";
+
+
+const databaseOptions: DatabaseOptions = {
+  sincronize: false,
+  migrationsRun: true,
+  storage: new DatabaseStorage({
+    key: "DATABASE",
+    index: 0,
+  }),
+  entities: ENTITIES,
+  migrations: ["/migrations/*.{ts,js}"],
+  logger: new ConsoleLogger(false),
+};
+
+
+let db: Database | undefined;
+
+
+export default Server(
+    async () => {
+      db = new Database(databaseOptions);
+      await db.load();
+      return CreateServer();
+    },
+    {
+      init: init([], async () => {
+        try {
+          db = new Database(databaseOptions);
+          await db.init();
+          setNodeServer(CreateServer());
+        } catch (error) {
+          console.error('Error initializing database:', error);
+          throw error;
+        }
+      }),
+      preUpgrade: preUpgrade(() => {
+        try {
+          if (!db) {
+            throw new Error('Database not initialized');
+          }
+  
+          db.save();
+        } catch (error) {
+          console.error('Error saving database:', error);
+        }
+      }),
+      postUpgrade: postUpgrade([], async () => {
+        try {
+          db = new Database(databaseOptions);
+          await db.load();
+          setNodeServer(CreateServer());
+        } catch (error) {
+          console.error('Error loading database:', error);
+        }
+      }),
+    },
+  );
+
---
+++ src/vibe_backend/server.ts
@@ -0,0 +1,25 @@
+import cors from "cors";
+import express from "express";
+
+import ExceptionHandler from "App/exceptions/handler";
+import { routes } from "./start/routes";
+
+export function CreateServer() {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json({ limit: "1.5mb" }));
+  app.use(ExceptionHandler);
+  app.use(routes);
+
+  // Guard routes
+  app.use((request, response) => {
+    response.status(404);
+    return response.json({
+      status: 0,
+      message: "Route not found!",
+    });
+  });
+
+  return app.listen();
+}
